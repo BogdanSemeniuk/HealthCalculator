13d498439ff883444e097731ea99dbf714637ee8
Богдан Семенюк <>
2021-06-14 11:07:33 +0300

    initial commit

---
+++ HealthCalculator/HealthCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthCalculatorApp.swift
+//  HealthCalculator
+//
+//  Created by Богдан Семенюк on 14.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct HealthCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
